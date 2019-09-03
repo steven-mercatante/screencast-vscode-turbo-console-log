4b25db1442cea3395ec11c16b9d2c3c7121aa7b7
Steven Mercatante <steven.mercatante@gmail.com>
2019-09-03 19:25:17 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const name = "Steven Mercatante";
+const isHungry = true;
+const wantsToEat = "kbbq";
+
+function greet() {
+  const msg = "hello, friend!";
+}
+
+class Person {
+  constructor() {
+    name = "Steven Mercatante";
+    isStillHungry = true;
+  }
+}
